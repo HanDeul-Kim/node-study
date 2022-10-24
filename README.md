5ed0548b095e3b9e9010194ff82d92cbeab2322c
KimHanDeul <dle1883@gmail.com>
2022-10-24 23:08:36 +0900

    study

---
+++ README.md
@@ -0,0 +1,2 @@
+# Node.js
+**node.js의 express study 레파지토리입니다.**
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // express 기본 셋팅
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended: true})) 
 app.listen(2020, (req, res) => {
     console.log('2020port')
 });
@@ -20,6 +21,9 @@ app.get('/write', (req, res) => {
     res.sendFile(__dirname + '/write.html')
 })
 app.post('/add', (req, res) => {
+    // console창에 출력만 하지말고 클라이언트가 보낸 데이터를 db에 영구적으로 저장을 해보자
+    console.log(req.body);
+    console.log(req.body.title);
     res.send('전송 완료!')
 })
 
