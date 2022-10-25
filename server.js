ab0043bfe77ec648a4bb9f04ee9b8c4281f1a262
KimHanDeul <dle1883@gmail.com>
2022-10-25 23:37:04 +0900

    mongodb error

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 // express 기본 셋팅
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({extended: true})) 
-app.listen(2020, (req, res) => {
-    console.log('2020port')
-});
+app.use(express.urlencoded({ extended: true }))
+const MongoClient = require('mongodb').MongoClient;
+var db;
+MongoClient.connect('mongodb+srv://beenzino13:1q2w3e4r@first-db.cuypcoh.mongodb.net/?retryWrites=true&w=majority', (err, client) => {
+    if(err) return console.log('에러~')
 
+    // todoapp database에 연결
+    db = client.db('todoapp');
+    db.collection('post').insertOne('저장할데이터', (err, res) => {
+        console.log('저장완료');
+    });
+    // db연결 되면 실행 할 코드 
+    app.listen(2020, () => {
+        console.log('2020port')
+    });
+
+})
+
+// app.listen(2020, (req, res) => {
+//     console.log('2020port')
+// });
 app.get('/music', (req, res) => {
     res.send('music 플랫폼 입니다.');
 })
