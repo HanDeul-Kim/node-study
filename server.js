22d7b62dc05d7d1c9e5c6345425860067689f4be
HanDeul-Kim <dle1883@gmail.com>
2022-12-10 21:27:12 +0900

    배포 전 마무리 단계

--- server.js
+++ server.js
@@ -75,10 +75,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('/login', (req, res) => {
-    let newArr = [];
+    const newArr = [];
     newArr.push(req.user);
-    res.render('index.ejs', { text: newArr });
-    res.render('login.ejs', { text: newArr})
+    console.log(newArr);
+    res.render('login.ejs', {text: newArr})
 })
 app.get('/fail', (req, res) => {
     res.render('fail.ejs')
@@ -96,38 +96,20 @@ app.post('/login', passport.authenticate('local', {
 
 // html 파일 보내보기
 app.get('/', (req, res) => {
-   
-    // res.render('index.ejs');
-
-    if(req.user == null) {
-        res.render('index.ejs', {text: 'Login'})
-        console.log('없다')
-    } else {
-        console.log('잇다')
-        let newArr = [];
-        newArr.push(req.user);
-        res.render('index.ejs', { text: newArr });
-    }
+    res.render('index.ejs');
 })
 
-app.get('/write', 로그인했니2, (req, res) => {
+app.get('/write', 로그인했니, (req, res) => {
     res.render('write.ejs');
 })
 // 로그인 상태관리 
 app.get('/mypage', 로그인했니, (req, res) => {
     console.log(req.user);
-    res.render('mypage.ejs', {사용자: req.user})
+    res.render('mypage.ejs', {user: req.user})
 })
 
 // 미들웨어
 function 로그인했니(req, res, next) {
-    if(req.user) {
-        next()
-    } else {
-        res.send('로그인 안되어있음.')
-    }
-}
-function 로그인했니2(req, res, next) {
     if(req.user) {
         next();
     } else {
@@ -136,7 +118,7 @@ function 로그인했니2(req, res, next) {
 }
 
 // ejs 파일 보내보기 (views라는 이름의 폴더를 만들고 넣어야함.)
-app.get('/list', 로그인했니2, (req, res) => {
+app.get('/list', 로그인했니, (req, res) => {
     // post라는 collection안의 모든 데이터를 꺼내보자
     db.collection('post').find().toArray((err, result) => {
         res.render('list.ejs', { todos: result });
@@ -208,7 +190,7 @@ app.post('/add', (req, res) => {
         })
     });
 
-    res.send('전송 완료!')
+    res.send(`<script>alert('todo 등록 완료!'); window.location.replace('/')</script>;`)
 })
 
 // delete 요청
